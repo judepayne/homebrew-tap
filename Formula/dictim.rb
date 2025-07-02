c14a9d194943b471756d297809816ee10bc89593
CircleCI <jude@judep.org>
2025-07-02 17:34:40 +0000

    Update dictim to version 0.9.0.4

--- Formula/dictim.rb
+++ Formula/dictim.rb
@@ -7,16 +7,16 @@ class Dictim < Formula
   
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/judepayne/dictim/releases/download/v0.9.0.4/dictim-0.9.0.4-macos-aarch64.zip"
-    sha256 "526831229d725f84b74b6eac6bbbd0a1fa82114fa68117cac6a3bfd185ed3828"
+    sha256 "af7106d17ef8a1ec64cee656eb60c28a1d6fc9ecf1ea62a4451be1a9cf9efbc9"
   elsif OS.mac? && Hardware::CPU.intel?
     url "https://github.com/judepayne/dictim/releases/download/v0.9.0.4/dictim-0.9.0.4-macos-amd64.zip"
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   elsif OS.linux? && Hardware::CPU.arm?
     url "https://github.com/judepayne/dictim/releases/download/v0.9.0.4/dictim-0.9.0.4-linux-aarch64.zip"
-    sha256 "1d3c5d4c9beaf69daaa70e5c229b37589bada4d6b059dbcb7ebb1f33d951a75a"
+    sha256 "3a61784bc57c1d6132ea06ed894a328ead239af5c2f20a9e7110fad236672345"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/judepayne/dictim/releases/download/v0.9.0.4/dictim-0.9.0.4-linux-amd64.zip"
-    sha256 "e291ead0d6c91af4aa47aa5b4bc90db44ffff4c8c7d965d7a9338cfebd8ef8b7"
+    sha256 "8421505d07a16eb08e5e377728d2fe6e06348dd7fc619a3b94bbf097fc37dd1f"
   end
 
   def install
