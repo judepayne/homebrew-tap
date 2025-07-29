25084dcdb02e79492ae26a11d93db4c4f5ba8ed6
CircleCI <jude@judep.org>
2025-07-29 04:39:28 +0000

    Update ednx to version 0.1.8

--- Formula/ednx.rb
+++ Formula/ednx.rb
@@ -1,17 +1,17 @@
 class Ednx < Formula
   desc "Go library and CLI tool for converting between EDN and JSON formats"
   homepage "https://github.com/judepayne/ednx"
-  version "0.1.7"
+  version "0.1.8"
   
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/judepayne/ednx/releases/download/0.1.7/ednx-macos-arm64.zip"
-    sha256 "2ccb5ff9774b85401a5f2f553efc034bd6da88e6314f0e787087d656bc6815e9"
+    url "https://github.com/judepayne/ednx/releases/download/0.1.8/ednx-macos-arm64.zip"
+    sha256 "a34c8845d8189860048fdeb96c10f884ca3c3f17c7d6836f3285b9ce8d0870ab"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/judepayne/ednx/releases/download/0.1.7/ednx-linux-arm64.zip"
-    sha256 "d8041682e28c8c9945b1ae529beaa39afa0b186ca724f315be7413aa74af200f"
+    url "https://github.com/judepayne/ednx/releases/download/0.1.8/ednx-linux-arm64.zip"
+    sha256 "667c2cbe156a4462f2db19006c5dce102eba8f8eef14d797d16f8b5fc1ba4d8e"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/judepayne/ednx/releases/download/0.1.7/ednx-linux-amd64.zip"
-    sha256 "7d255249fd17cf3a57126d4ef4f614232c80d3b5c6df0b33cc298e86094cf8cf"
+    url "https://github.com/judepayne/ednx/releases/download/0.1.8/ednx-linux-amd64.zip"
+    sha256 "66dffb7ac952540d18fe3cae45aca578acc52e16e573ea636d24f8135dbf1e49"
   end
 
   def install
